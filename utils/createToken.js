580f2a278b98a184f02ea77b7ca9e8d688368573
Mohit Saini <msmohit1612@gmail.com>
2025-03-09 02:47:12 +0530

    add a console

--- utils/createToken.js
+++ utils/createToken.js
@@ -4,7 +4,7 @@ const generateToken = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "30d",
   });
-
+  console.log("secure:", process.env.NODE_ENV !== "development");
   // Set JWT as an HTTP-Only Cookie
   res.cookie("jwt", token, {
     httpOnly: true,
