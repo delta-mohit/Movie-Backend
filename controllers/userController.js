6d211452d71ef1c8afdd7bc0653bf7391fb22103
Mohit Saini <msmohit1612@gmail.com>
2025-03-09 12:46:41 +0530

    remove security from cookies and allow all origin

--- controllers/userController.js
+++ controllers/userController.js
@@ -64,7 +64,6 @@ const loginUser = asyncHandler(async (req, res) => {
 
 const logoutCurrentUser = asyncHandler(async (req, res) => {
   res.cookie("jwt", "", {
-    httpOnly: true,
     expires: new Date(0),
   });
 
--- utils/createToken.js
+++ utils/createToken.js
@@ -7,9 +7,6 @@ const generateToken = (res, userId) => {
   console.log("secure:", process.env.NODE_ENV !== "development");
   // Set JWT as an HTTP-Only Cookie
   res.cookie("jwt", token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
-    sameSite: "strict",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 
